6d54edbdc6c9b0afc8f103e302cf10e1d04c0eec
Jamie Eisenhart <jamie@jamieei.org>
2011-12-19 22:38:32 -0800

    playable

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -6,6 +6,17 @@ class Game(word: String, guesses: Set[Char]) {
 	def this(word: String) = this(word, new HashSet[Char]())
 
   def maskedWord(): String = word.map((ch: Char) => if (guesses.contains(ch)) ch else '_')
-  override def toString = maskedWord //.reduceLeft(_ + " " ++ _)
+  override def toString = maskedWord.foldLeft("")((acc, c) => acc + ' ' + c)
+
+  def missingLetters: Set[Char] = word.toSet.diff(guesses)
+  def wrongGuesses: Set[Char] = guesses.diff(word.toSet)
+  def nWrong = wrongGuesses.size
+  def nRight = guesses.size - nWrong
+
+  def next(guess: Char): Game = new Game(word, guesses + guess)
+
+  def isWin: Boolean = missingLetters.isEmpty
+  def isLoss: Boolean = (nWrong > 6)
+  def isOver: Boolean = (isWin || isLoss)
 }
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,20 @@ package org.jamieei.hangman
 
 object Main {
   def main(args: Array[String]) {
-  	println(new Game("test").toString())
+    var game = new Game("test")
+    while (!game.isOver) {
+      println(game)
+      print("Next guess (Q to quit): ")
+      
+      val ch = readChar
+      println(ch)
+      println
+
+      if (ch == 'Q') return
+      game = game.next(ch)
+    }
+
+    if (game.isWin) println("Sweet victory!!!")
+    if (game.isLoss) println("Crhusing defeat!!!")
   }
 }
