997aa610ccdc02e556ca79e6bf9dc26721ca8431
Jamie Eisenhart <jamie@jamieei.org>
2011-12-19 23:05:57 -0800

    rules working

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -3,10 +3,16 @@ package org.jamieei.hangman
 import collection.immutable._
 
 class Game(word: String, guesses: Set[Char]) {
-	def this(word: String) = this(word, new HashSet[Char]())
+	def this(word: String) = this(word, new TreeSet[Char]())
+
+	private val figures = List("", "o", "o-", "oc", "oE", "oE\\", "oE<")
+  private def spacify(str: String): String = str.foldLeft("")((acc, c) => acc + ' ' + c)
 
   def maskedWord(): String = word.map((ch: Char) => if (guesses.contains(ch)) ch else '_')
-  override def toString = maskedWord.foldLeft("")((acc, c) => acc + ' ' + c)
+  override def toString = 
+    spacify(maskedWord) + "    " +
+    figures(nWrong) + "    " +
+    spacify(wrongGuesses.mkString)
 
   def missingLetters: Set[Char] = word.toSet.diff(guesses)
   def wrongGuesses: Set[Char] = guesses.diff(word.toSet)
@@ -16,7 +22,7 @@ class Game(word: String, guesses: Set[Char]) {
   def next(guess: Char): Game = new Game(word, guesses + guess)
 
   def isWin: Boolean = missingLetters.isEmpty
-  def isLoss: Boolean = (nWrong > 6)
+  def isLoss: Boolean = (nWrong >= (figures.size - 1))
   def isOver: Boolean = (isWin || isLoss)
 }
 
